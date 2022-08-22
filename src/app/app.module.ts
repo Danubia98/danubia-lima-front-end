330f7c9e8f9989cd089d779f51069eae52d434ab
Danubia de Sobral Lima <danubia.lima@gpnet.com.br>
2022-08-22 14:16:57 -0300

    Criando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { ListarAutorComponent } from './components/listar-autor/listar-autor.component';
+import { ListarLivrosComponent } from './components/listar-livros/listar-livros.component';
+import { CadastrarAutorComponent } from './components/cadastrar-autor/cadastrar-autor.component';
+import { CadastrarLivroComponent } from './components/cadastrar-livro/cadastrar-livro.component';
+import { EditarAutorComponent } from './components/editar-autor/editar-autor.component';
+import { EditarLivroComponent } from './components/editar-livro/editar-livro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarAutorComponent,
+    ListarLivrosComponent,
+    CadastrarAutorComponent,
+    CadastrarLivroComponent,
+    EditarAutorComponent,
+    EditarLivroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
